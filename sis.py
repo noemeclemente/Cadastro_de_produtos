e9bd008e9074cb43a0cd27077c4f1c1c3470b880
Noeme Clemente <noeme.clemente@outlook.com>
2024-05-13 20:52:12 -0300

    Adicionando primeiras fases do código
    Nessa fase o código já está conectado ao banco, possui função de listar os itens e de gerar pdf.

---
+++ sis.py
@@ -0,0 +1,99 @@
+import mysql.connector
+from PyQt5 import QtWidgets, uic
+import reportlab
+from reportlab.pdfgen import canvas
+
+banco = mysql.connector.connect(
+    host ="localhost",
+    user="root",
+    passwd="Heyjude70*",
+    database="cadastro_produtos"
+)
+
+def gerar_pdf():
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+    y = 0
+    pdf = canvas.Canvas("cadastro_produtos.pdf")
+    pdf.setFont("Times-Bold", 25)
+    pdf.drawString(200, 800, "Produtos cadastrados:")
+    pdf.setFont("Times-Bold", 18)
+
+    pdf.drawString(10, 750, "ID")
+    pdf.drawString(110, 750, "CODIGO")
+    pdf.drawString(210, 750, "PRODUTO")
+    pdf.drawString(310, 750, "PREÇO")
+    pdf.drawString(410, 750, "CATEGORIA")
+
+    for i in range(0, len(dados_lidos)):
+        y = y + 30  # Incremento da linha para evitar sobreposição
+        pdf.drawString(10, 750 - y, str(dados_lidos[i][0]))
+        pdf.drawString(110, 750 - y, str(dados_lidos[i][1]))
+        pdf.drawString(210, 750 - y, str(dados_lidos[i][2]))
+        pdf.drawString(310, 750 - y, str(dados_lidos[i][3]))
+        pdf.drawString(410, 750 - y, str(dados_lidos[i][4]))
+
+    pdf.save()
+    print("PDF FOI GERADO COM SUCESSO!")
+
+
+def funcao_principal():
+    linha1 = formulario.lineEdit.text()
+    linha2 = formulario.lineEdit_2.text()
+    linha3 = formulario.lineEdit_3.text()
+
+    categoria = ""
+
+    if formulario.radioButton.isChecked():
+        print("Categoria Eletronicos selecionada")
+        categoria = "Informatica"
+    elif formulario.radioButton_2.isChecked():
+        print("Categoria Informatica selecionada")
+        categoria = "Alimentos"
+    else:
+        print("Categoria Alimentos selecionada")
+        categoria = "Eletrônicos"
+
+    print("Código:", linha1)
+    print("Descricao:", linha2)
+    print("Preco", linha3)
+
+    cursor = banco.cursor()
+    comando_SQL = "INSERT INTO produtos (codigo,descricao,preco,categoria) VALUES (%s,%s,%s,%s)"
+    dados = (str(linha1),str(linha2),str(linha3),categoria)
+    cursor.execute(comando_SQL,dados)
+    banco.commit()
+    formulario.lineEdit.setText("")
+    formulario.lineEdit_2.setText("")
+    formulario.lineEdit_3.setText("")
+
+segunda_tela = None
+
+def chama_segunda_tela():
+    global segunda_tela
+    if segunda_tela is None:
+        segunda_tela = uic.loadUi("listardados.ui")
+        segunda_tela.pushButton.clicked.connect(gerar_pdf)
+    segunda_tela.show()
+
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall() # pega o que foi feiro na última linha do cursor
+
+    segunda_tela.tableWidget.setRowCount(len(dados_lidos))
+    segunda_tela.tableWidget.setColumnCount(5)
+
+    for i in range(0, len(dados_lidos)):
+        for j in range(0,5):
+            segunda_tela.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+
+app = QtWidgets.QApplication([])
+formulario = uic.loadUi("formulario.ui")
+formulario.pushButton.clicked.connect(funcao_principal)
+formulario.pushButton_2.clicked.connect(chama_segunda_tela)
+
+formulario.show()
+app.exec()
